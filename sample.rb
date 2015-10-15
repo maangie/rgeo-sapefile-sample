3edcbc8264d9b5fcf5a467fc3d5aa19331fe5bf1
maangie <maangie906@gmail.com>
2015-10-15 13:27:30 +0900

    create sample

---
+++ sample.rb
@@ -0,0 +1,13 @@
+require 'rgeo/shapefile'
+
+RGeo::Shapefile::Reader.open('hakata-shp/h22ka40132.shp') do |file|
+  puts "File contains #{file.num_records} records."
+  file.each do |record|
+    puts "Record number #{record.index}:"
+    puts "  Geometry: #{record.geometry.as_text}"
+    puts "  Attributes: #{record.attributes.inspect}"
+  end
+  file.rewind
+  record = file.next
+  puts "First record geometry was: #{record.geometry.as_text}"
+end
